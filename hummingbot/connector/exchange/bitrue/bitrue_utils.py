02df898962a8e45b8005a2572ad642b8fbcf7f02
Alexey Grachev <alxgrh@yandex.ru>
2020-10-12 12:45:17 +0300

    Update Bitrue utils

--- hummingbot/connector/exchange/bitrue/bitrue_utils.py
+++ hummingbot/connector/exchange/bitrue/bitrue_utils.py
@@ -3,24 +3,53 @@ from hummingbot.core.event.events import (
     OrderType,
     TradeType
 )
-from bitrue.client import Client as BitrueAPIClient
+import re
+from typing import (
+    Optional,
+    Tuple)
 
-HBOT_BROKER_ID = "HBOT-"
+
+TRADING_PAIR_SPLITTER = re.compile(r"^(\w+)(BTC|USDT|XRP|ETH)$")
+
+HBOT_BROKER_ID = "HB-"
 
 
 def get_new_client_order_id(side: str, trading_pair: str) -> str:
-    return f"{HBOT_BROKER_ID}{side}-{trading_pair}-{get_tracking_nonce()}"
+    side = 'S' if side == 'SELL' else 'B'
+    return f"{trading_pair}-{side}{get_tracking_nonce()}"
 
 
-def get_bitrue_trade_type(trade_type: TradeType):
+def get_bitrue_trade_type(trade_type: TradeType) -> str:
     hb_to_exchange_mapping = {
-        TradeType.SELL: BitrueAPIClient.SIDE_SELL,
-        TradeType.BUY: BitrueAPIClient.SIDE_BUY}
+        TradeType.SELL: 'SELL',
+        TradeType.BUY: 'BUY'}
     return hb_to_exchange_mapping[trade_type]
 
 
-def get_bitrue_order_type(order_type: OrderType):
+def get_bitrue_order_type(order_type: OrderType) -> str:
     hb_to_exchange_mapping = {
-        OrderType.LIMIT: BitrueAPIClient.ORDER_TYPE_LIMIT,
-        OrderType.MARKET: BitrueAPIClient.ORDER_TYPE_MARKET}
+        OrderType.LIMIT: 'LIMIT',
+        OrderType.MARKET: 'MARKET'}
     return hb_to_exchange_mapping[order_type]
+
+
+def split_trading_pair(trading_pair: str) -> Optional[Tuple[str, str]]:
+    try:
+        m = TRADING_PAIR_SPLITTER.match(trading_pair)
+        return m.group(1), m.group(2)
+    # Exceptions are now logged as warnings in trading pair fetcher
+    except Exception:
+        return None
+
+
+def convert_from_exchange_trading_pair(exchange_trading_pair: str) -> Optional[str]:
+    if split_trading_pair(exchange_trading_pair) is None:
+        return None
+
+    base_asset, quote_asset = split_trading_pair(exchange_trading_pair)
+    return f"{base_asset}-{quote_asset}"
+
+
+def convert_to_exchange_trading_pair(hb_trading_pair: str) -> str:
+    # Binance does not split BASEQUOTE (BTCUSDT)
+    return hb_trading_pair.replace("-", "")
